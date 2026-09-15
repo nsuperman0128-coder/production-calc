e8a21038f19d236a153894ab69b920efc1b07eb8
nsuperman0128-coder <n.super.man.0128@gmail.com>
2026-09-15 13:07:58 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,15 @@
-const C='prodcalc-v2';const A=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='molding-calc-suite-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))).then(()=>self.skipWaiting()));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+).then(()=>self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(cached=>{
+    const network=fetch(e.request).then(resp=>{
+      const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
+    }).catch(()=>cached);
+    return cached || network;
+  }));
+});
